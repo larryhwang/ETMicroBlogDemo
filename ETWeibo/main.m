8d431f9f0afb1e10e1b394d1416e2eb6a652d817
Larry <Larry@Larry-MacBook-Pro.local>
2015-08-19 09:30:50 +0800

    Initial Commit

---
+++ ETWeibo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ETWeibo
+//
+//  Created by Larry on 15/8/19.
+//  Copyright (c) 2015年 Larry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
